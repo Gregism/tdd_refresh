ebf13a1570e1e84f7120bb02eab673894635da2d
Gregory Smith <gregorysmith@Gregorys-MacBook-Pro.local>
2019-02-11 12:35:15 -0600

    Begin form submission

--- functional_tests.py
+++ functional_tests.py
@@ -36,14 +36,20 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows), 
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feathers' , [row.text for row in rows])
 	
 		#There is still a text box for a new item
 		#Bob enters "Use feather to make a fly"
-		#
+		inputbox.send_keys('Use feathers to make a fly')
+		#When he hits enter, the page updates and lists
+		#1: Buy peacock feathers
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers' , [row.text for row in rows])
+		self.assertIn('2: Use feathers to make a fly' , [row.text for row in rows])
 		#The page updates again and shows both items in the list
 		#
 		#Bob wonders if the site will remember the list, then he sees that the site
