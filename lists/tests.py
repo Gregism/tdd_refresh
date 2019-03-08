66af8a35afe066437b9c1a4b1577c916ac68938c
Gregory Smith <gregory.terrell.smith@gmail.com>
2019-03-08 15:47:50 -0600

    New View and Template added

--- lists/tests.py
+++ lists/tests.py
@@ -7,16 +7,18 @@ from lists.views import home_page
 from lists.models import Item
 
 class ListViewTest(TestCase):
+	def test_uses_list_template(self):
+		response = self.client.get('/lists/the-only-list-in-the-world/')
+		self.assertTemplateUsed(response, 'list.html')
+
 	def test_displays_all_items(self):
 		Item.objects.create(text='itemey 1')
 		Item.objects.create(text='itemey 2')
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
-
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
-
-
+	
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
